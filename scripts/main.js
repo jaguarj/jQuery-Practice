d44d00a0512966c25a30773c2e0fee66342f4dab
Josh Seipel <joshseipel@gmail.com>
2017-05-22 16:28:42 -0400

    Initial test passed.

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,28 @@
 console.log("We are connected");
+
+$(document).ready(function() {
+
+
+$("#box1").click(function(){
+	$("#box1").html("hello Josh! You are the best!");
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+});
+
