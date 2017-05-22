284b19382aebebfb79eccac4b1942a000d1c83ed
Josh Seipel <joshseipel@gmail.com>
2017-05-22 17:05:12 -0400

    Added a button to hide and show (toggle).

--- scripts/main.js
+++ scripts/main.js
@@ -1,10 +1,16 @@
-console.log("We are connected");
-
 $(document).ready(function() {
+console.log("We are connected!");
+// Start your scripts here.
+
+// $("#box1").click(function(){
+// 	$("#box1").toggle("hello Josh! You are the best!");
+// });
 
 
-$("#box1").click(function(){
-	$("#box1").html("hello Josh! You are the best!");
+// Add a class to an element //
+
+$("button").click(function(){
+	$("#box1").toggle("box1");
 });
 
 
