eca7c4828e2f8726c13a6142d8f9fd6e6ebed9d4
Josh Seipel <joshseipel@gmail.com>
2017-05-29 17:34:08 -0400

    Added .each() to iterate over divs to change color on click.

--- scripts/main.js
+++ scripts/main.js
@@ -2,19 +2,35 @@ $(document).ready(function() {
 console.log("We are connected!");
 // Start your scripts here.
 
-// $("#box1").click(function(){
-// 	$("#box1").toggle("hello Josh! You are the best!");
-// });
+$("#btn1").click(function(){
+	$("#box1").toggle("hello Josh! You are the best!");
+});
 
 
-// Add a class to an element //
+// Add a toggle to first element. //
 
-$("button").click(function(){
+$("btn1").click(function(){
 	$("#box1").toggle("box1");
 });
 
 
+// Add a toggle to second group elements. //
 
+
+$("#btn2").click(function(){
+	$("#box2").toggle("fast");
+});
+
+
+$(document.body).click(function() {
+	$("div.iterate").each(function(){
+		if (this.style.background !== "lightblue") {
+			this.style.background = "lightblue";
+		} else {
+			this.style.background = "";
+		}
+	});
+});
 
 
 
